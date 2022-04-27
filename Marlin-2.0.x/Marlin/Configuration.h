c75f1821ce685c2e1f34eef044ac4da577eef910
pierre geeraert <pierre.geeraert@viacesi.fr>
2022-04-27 14:54:06 +0200

    apply SKR config on the good branch v2

--- Marlin-2.0.x/Marlin/Configuration.h
+++ Marlin-2.0.x/Marlin/Configuration.h
@@ -69,7 +69,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Pierre, default config)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Pierre, SKR v_1.4 turbo config)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
