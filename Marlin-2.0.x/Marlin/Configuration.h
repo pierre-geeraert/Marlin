760792cffc909d1f83ad23c3cdc2957dfd045692
pierre geeraert <pierre.geeraert@viacesi.fr>
2022-04-27 14:55:38 +0200

    apply Arduino mega 2560 config on the good branch

--- Marlin-2.0.x/Marlin/Configuration.h
+++ Marlin-2.0.x/Marlin/Configuration.h
@@ -70,7 +70,6 @@
 
 // Author info of this build printed to the host during boot and M115
 #define STRING_CONFIG_H_AUTHOR "(Pierre, Arduino mega 2560 config)" // Who made the changes.
-//branch mega
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
